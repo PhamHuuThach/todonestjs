0c97592863b92a89cadb69767fa578de559798dd
PhamHuuThach <thachhthlqt@gmail.com>
2026-01-18 16:24:37 +0700

    add file env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,29 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Todo } from "./todos/entities/todo.entity";
 import { Category } from "./categories/entities/category.entity";
 import { CategoriesModule } from "./categories/categories.module";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// @ts-ignore
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: "mongodb",
-      url: "mongodb+srv://phamhuuthach:0326430775Thach@cluster0.jku7kpn.mongodb.net/todosdb",
+      url: process.env.DATABASE_URL,
+      database: process.env.DATABASE_NAME,
       entities: [Todo, Category],
       synchronize: true,
     }),
     TodosModule,
     CategoriesModule,
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
