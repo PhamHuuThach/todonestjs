cea5be3223c42801565285d10971d453ada1a849
PhamHuuThach <thachhthlqt@gmail.com>
2026-01-18 23:10:53 +0700

    abccccc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from "@nestjs/common";
 import { TodosModule } from "./todos/todos.module";
+import { CategoriesModule } from "./categories/categories.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Todo } from "./todos/entities/todo.entity";
 import { Category } from "./categories/entities/category.entity";
-import { CategoriesModule } from "./categories/categories.module";
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
-// @ts-ignore
 import { ConfigModule } from "@nestjs/config";
 
 @Module({
@@ -16,17 +11,24 @@ import { ConfigModule } from "@nestjs/config";
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+
     TypeOrmModule.forRoot({
       type: "mongodb",
       url: process.env.DATABASE_URL,
       database: process.env.DATABASE_NAME,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+
+      // 2 tuỳ chọn TLS/SSL để kết nối Atlas
+      ssl: true,
+      sslValidate: false, // ⚠️ chỉ dùng khi SSL verify gây lỗi
+
       entities: [Todo, Category],
       synchronize: true,
     }),
+
     TodosModule,
     CategoriesModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
